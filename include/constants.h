3394ba0abd77c84a230b1a89590c066f09b01acf
Gustave Monce <gustave.monce@outlook.com>
2024-07-17 19:17:26 +0200

    Move static provisioning data to a specific file

---
+++ include/constants.h
@@ -0,0 +1,14 @@
+// Copyright (c) DuoWoA authors. All Rights Reserved.
+
+#pragma once
+
+#include <ntddk.h>
+#include <wdf.h>
+#include <windef.h>
+
+extern BYTE WLAN_SAR2CFG_PROVISION[42];
+extern BYTE WLAN_CLPC_PROVISION[26328];
+extern BYTE WLAN_PROVISION[9];
+extern BYTE BT_PROVISION[8];
+extern BYTE BT_NVMTAG83_PROVISION[10];
+extern BYTE BT_NVMTAG36_PROVISION[18];
